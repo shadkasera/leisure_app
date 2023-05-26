d2303904f1a307f6f16a35ff43067c4b21da9cee
Shadreck kasera <shadkasera@gmail.com>
2023-05-26 09:52:52 +0200

    updated files

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+  const WelcomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,8 @@ class WelcomePage extends StatelessWidget {
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(
-                    'assets/images/pexels-jess-loiterton-5006893.jpg'),
+                  'assets/images/pexels-jess-loiterton-5006893.jpg',
+                ),
                 fit: BoxFit.cover,
               ),
             ),
@@ -20,19 +21,50 @@ class WelcomePage extends StatelessWidget {
           Center(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: RichText(
-                text: const TextSpan(
-                  style: TextStyle(fontSize: 50),
-                  children: [
-                    TextSpan(
-                        text: 'WELCOME TO ', style: TextStyle(color: Colors.black)),
-                    TextSpan(
-                        text: 'LEISURE EASILY ',
-                        style: TextStyle(color: Colors.black)),
-                    TextSpan(
-                        text: 'MALAWI APP', style: TextStyle(color: Colors.black)),
-                  ],
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(fontSize: 30),
+                      children: [
+                        TextSpan(
+                          text: 'WELCOME TO ',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        TextSpan(
+                          text: 'LEISURE EASILY ',
+                          style: TextStyle(color: Colors.black, fontSize: 50),
+                        ),
+                        TextSpan(
+                          text: 'MALAWI APP',
+                          style: TextStyle(color: Colors.black, fontSize: 50),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        // Button pressed callback
+                      },
+                      child: const Text(
+                        'Get started',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
