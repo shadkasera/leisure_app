a6994bea297d2a4c22ae8f169f636862fee917bc
Shadreck kasera <shadkasera@gmail.com>
2023-05-26 12:07:28 +0200

    updated files

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,8 +10,36 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    size: 30,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.person,
+                    size: 80,
+                  ),
+                ),
+              ],
+            ),
             Container(
-              width: 200,
+              width: 300,
               height: 40,
               decoration: BoxDecoration(
                 border: Border.all(
@@ -23,12 +51,16 @@ class HomePage extends StatelessWidget {
               child: const TextField(
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  hintText: 'Enter your text',
+                  hintText: 'Search the area',
                   hintStyle: TextStyle(fontSize: 20),
                   contentPadding: EdgeInsets.symmetric(horizontal: 10),
                   prefixIcon: Icon(Icons.search),
                 ),
               ),
+            ),
+            const Text(
+              'Where do you wanna go?',
+              style: TextStyle(fontSize: 32),
             ),
             const SizedBox(height: 20),
             Row(
